8fb0729d12159d8a156fbedf7f93b1352601cd33
Sam Ghatak <samanwayghatak@gmail.com>
2024-12-17 19:08:06 +0530

    Added Booking Sys Admin app.

---
+++ booking-servers/Booking.SysAdmin.Web/Controllers/TenantController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.SysAdmin.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TenantController : ControllerBase
+    {
+        //public async Task<Tenant> CreateTenant(Tenant tenant)
+        //{
+        //    return await _tenantService.CreateTenant(tenant);
+        //}
+    }
+}
